b762ee1967d414cb2c0df515b838df467537a756
sharet9446 <spees97@naver.com>
2025-03-19 10:20:16 +0900

    챔피언 상세 페이지에서 동적 파라미터 사용 설정 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -13,6 +13,8 @@ import { Metadata } from "next";
 import Image from "next/image";
 import React from "react";
 
+export const dynamicParams = true;
+
 export async function generateMetadata({
   params,
 }: ChampionDetailProps): Promise<Metadata> {
